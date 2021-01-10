1bcad1dce98d1d57ebacee25c33e4918c5139849
aselivanov <aseliv@gmail.com>
2021-01-10 05:43:37 +0300

    Day 17 part 2.

--- src/seliv/aoc/aoc2020/Aoc17.java
+++ src/seliv/aoc/aoc2020/Aoc17.java
@@ -15,7 +15,7 @@ public class Aoc17 {
             String s = ops.get(y);
             for (int x = 0 ; x < s.length(); x++) {
                 if (s.charAt(x) == '#') {
-                    Point p = new Point(x, y, 0);
+                    Point p = new Point(x, y, 0, 0);
                     board.put(p, true);
                 }
             }
@@ -29,18 +29,20 @@ public class Aoc17 {
 
     public static Map<Point, Boolean> doIter(Map<Point, Boolean> board) {
         Map<Point, Boolean> newBorad = new HashMap<>();
-        for (int z = -50; z < 50; z++) {
-            for (int y = -50; y < 50; y++) {
-                for (int x = -50; x < 50; x++) {
-                    Point p = new Point(x, y, z);
-                    int c = countNeighbors(board, x, y, z);
-                    if (board.containsKey(p)) {
-                        if ((c == 2) || (c == 3)) {
-                            newBorad.put(p, true);
-                        }
-                    } else {
-                        if (c == 3) {
-                            newBorad.put(p, true);
+        for (int z = -20; z < 20; z++) {
+            for (int y = -20; y < 20; y++) {
+                for (int x = -20; x < 20; x++) {
+                    for (int w = -20; w < 20; w++) {
+                        Point p = new Point(x, y, z, w);
+                        int c = countNeighbors(board, x, y, z, w);
+                        if (board.containsKey(p)) {
+                            if ((c == 2) || (c == 3)) {
+                                newBorad.put(p, true);
+                            }
+                        } else {
+                            if (c == 3) {
+                                newBorad.put(p, true);
+                            }
                         }
                     }
                 }
@@ -49,16 +51,18 @@ public class Aoc17 {
         return newBorad;
     }
 
-    public static int countNeighbors(Map<Point, Boolean> board, int x, int y, int z) {
+    public static int countNeighbors(Map<Point, Boolean> board, int x, int y, int z, int w) {
         int res = 0;
         for (int dz = - 1; dz <= 1; dz++) {
             for (int dy = -1; dy <= 1; dy++) {
                 for (int dx = - 1; dx <=1; dx++) {
-                    if ((dx == 0) && (dy == 0) && (dz == 0)) {
-                        continue;
-                    }
-                    if (board.containsKey(new Point(x + dx, y + dy, z + dz))) {
-                        res++;
+                    for (int dw = - 1; dw <=1; dw++) {
+                        if ((dx == 0) && (dy == 0) && (dz == 0) && (dw == 0)) {
+                            continue;
+                        }
+                        if (board.containsKey(new Point(x + dx, y + dy, z + dz, w + dw))) {
+                            res++;
+                        }
                     }
                 }
             }
@@ -67,12 +71,13 @@ public class Aoc17 {
     }
 
     public static class Point {
-        public final int x, y, z;
+        public final int x, y, z, w;
 
-        public Point(int x, int y, int z) {
+        public Point(int x, int y, int z, int w) {
             this.x = x;
             this.y = y;
             this.z = z;
+            this.w = w;
         }
 
         @Override
@@ -84,6 +89,7 @@ public class Aoc17 {
 
             if (x != point.x) return false;
             if (y != point.y) return false;
+            if (w != point.w) return false;
             return z == point.z;
         }
 
@@ -92,6 +98,7 @@ public class Aoc17 {
             int result = x;
             result = 31 * result + y;
             result = 31 * result + z;
+            result = 31 * result + w;
             return result;
         }
     }
