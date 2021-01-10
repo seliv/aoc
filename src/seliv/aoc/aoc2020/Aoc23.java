57de8b6c73880f46714a1112dd647eb8428c6bd7
aselivanov <aseliv@gmail.com>
2021-01-10 06:13:07 +0300

    Day 23 part 2.

--- src/seliv/aoc/aoc2020/Aoc23.java
+++ src/seliv/aoc/aoc2020/Aoc23.java
@@ -9,15 +9,119 @@ public class Aoc23 {
     public static void main(String[] args) {
         String in = IN2;
 
+        int all = 1_000_000;
+
+        Node[] byValue = new Node[all + 1];
+
+        Node last = null;
+        Node prev = null;
+        for (int i = in.length() - 1; i >= 0; i--) {
+            final int v = Integer.parseInt("" + in.charAt(i));
+            Node n = new Node(v);
+            byValue[v] = n;
+            if (last == null) {
+                last = n;
+                prev = n;
+            } else {
+                n.next = prev;
+                prev = n;
+            }
+        }
+        for (int v = in.length() + 1; v <= all; v++) {
+            Node n = new Node(v);
+            byValue[v] = n;
+            last.next = n;
+            last = n;
+        }
+        last.next = prev;
+
+        Node now = prev;
+
+        for (int m = 0; m < 10_000_000; m++) {
+//        for (int m = 0; m < 10; m++) {
+            if (m % 100_000 == 0) {
+                print(all, now);
+            }
+//            print(all, now);
+
+            Node n1 = now.next;
+            Node n2 = n1.next;
+            Node n3 = n2.next;
+
+            int dest = now.v;
+
+            do {
+                dest--;
+                if (dest == 0) {
+                    dest = all;
+                }
+            } while (
+                    (dest == n1.v) ||
+                    (dest == n2.v) ||
+                    (dest == n3.v)
+            );
+
+//            Node f = n3.next;
+//            while (f.v != dest) {
+//                f = f.next;
+//            }
+            Node f = byValue[dest];
+
+            now.next = n3.next;
+            n3.next = f.next;
+            f.next = n1;
+
+            now = now.next;
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        print(all, now);
+    }
+
+    private static void print(int all, Node nn) {
+        while (nn.v != 1) {
+            nn = nn.next;
+        }
+        long res = (long)nn.next.v * (long)nn.next.next.v;
+        for (int i = 0; i < Math.min(all, 20); i++) {
+            System.out.print(" " + nn.v);
+            nn = nn.next;
+        }
+        System.out.println(" ( " + res + " )");
+        System.out.println();
+    }
+
+    static class Node {
+        public final int v;
+        public Node next;
+
+        public Node(int v) {
+            this.v = v;
+        }
+    }
+
+
+    public static void main1(String[] args) {
+        String in = IN0;
+
         LinkedList<Integer> q = new LinkedList<>();
         for (int i = 0; i < in.length(); i++) {
             q.add(Integer.parseInt("" + in.charAt(i)));
         }
+//        for (int i = 10; i < 1_00; i++) {
+//            q.add(i);
+//        }
 
         int idx = 0;
-        int all = 9;
+        int all = q.size() - 1;
 
         for (int m = 0; m < 100; m++) {
+//            if (m % 1_00_000 == 0) {
+//                System.out.println("m = " + m);
+//                System.out.println("q = " + q);
+//            }
             System.out.println("q = " + q);
 
             int i1 = q.get((idx + 1) % all);
@@ -26,7 +130,7 @@ public class Aoc23 {
 
             int dest = q.get(idx) - 1;
             if (dest < 1) {
-                dest = 9;
+                dest = all;
             }
 
             while (
@@ -40,7 +144,7 @@ public class Aoc23 {
                 }
             }
 
-            System.out.println("dest = " + dest);
+//            System.out.println("dest = " + dest);
 
             int f = q.poll();
             q.add(f);
@@ -85,6 +189,9 @@ public class Aoc23 {
         System.out.println();
         System.out.println("q = " + q);
     }
+
+    public static String IN0 = "123456789";
+
 
     public static String IN = "389125467";
 
